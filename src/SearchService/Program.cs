53856ce44554f5957dd44638e000129e1b139881
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-01-30 18:53:20 +0800

    End of section 3

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,14 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+// add polly policy
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -12,13 +16,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// 用这种方式可以避免 当auction 服务停止时，程序一直停在 DbInitializer.InitDb, 一直走不到 app.Run()
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() =>
+    HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3)); // try every 3 seconds
