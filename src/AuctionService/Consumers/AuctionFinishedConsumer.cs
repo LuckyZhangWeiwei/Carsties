d91a1e7c5b70752b5f9758667d81a911ae7c59ff
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-02-10 13:09:16 +0800

    completed 6-07

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private AuctionDbContext _auctionDbContext;
+
+    public AuctionFinishedConsumer(AuctionDbContext auctionDbContext)
+    {
+        _auctionDbContext = auctionDbContext;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("-----> consuming AuctionFinishedConsumer ");
+        
+        var auction = await _auctionDbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SolidAmont = context.Message.Amout;
+        }
+
+        auction.Status =
+            auction.SolidAmont > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+        await _auctionDbContext.SaveChangesAsync();
+    }
+}
---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,33 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private AuctionDbContext _auctionDbContext;
+
+    public BidPlacedConsumer(AuctionDbContext auctionDbContext)
+    {
+        _auctionDbContext = auctionDbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("----> consuming bid placed");
+
+        var auction = await _auctionDbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (
+            auction.CurrentHighBid == null
+            || context.Message.BidStatus.Contains("Accepted")
+                && context.Message.Amount > auction.CurrentHighBid
+        )
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _auctionDbContext.SaveChangesAsync();
+        }
+    }
+}
