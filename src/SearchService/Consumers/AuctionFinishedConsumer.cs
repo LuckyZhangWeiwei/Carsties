fe50ad847afd89f31806eae6a146fa9ae39255b8
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-02-10 15:20:34 +0800

    completed 6-08

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if(context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SolidAmont = (int)context.Message.Amout;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
