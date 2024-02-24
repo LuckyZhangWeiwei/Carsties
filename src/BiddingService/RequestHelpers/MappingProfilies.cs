9f55fefda80df3753b6575a1e8d0ea28696c8efb
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-02-24 11:40:42 +0800

    changed

--- src/BiddingService/RequestHelpers/MappingProfilies.cs
+++ src/BiddingService/RequestHelpers/MappingProfilies.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using AutoMapper;
+using Contracts;
+
+namespace BiddingService;
+
+public class MappingProfilies : Profile
+{
+    public MappingProfilies()
+    {
+        CreateMap<Bid, BidDto>();
+        CreateMap<Bid, BidPlaced>();
+    }
+}
