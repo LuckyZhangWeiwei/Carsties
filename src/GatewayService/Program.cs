57923af153266a389f17dd123cf5c2d1f3b3e249
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-02-25 13:51:14 +0800

    completed chapter  12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,7 +14,23 @@ builder.Services
         opt.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(
+        "customPolicy",
+        b =>
+        {
+            b.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins(builder.Configuration["ClientApp"]);
+        }
+    );
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
